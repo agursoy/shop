71a177b24170d2cea57c57324aa8b2a3f2922440
Abdullah Gürsoy <a.gursoy@msn.com>
2020-10-30 14:42:48 +0300

    lint

--- frontend/components/Buttons.vue
+++ frontend/components/Buttons.vue
@@ -1,41 +1,46 @@
 <template>
-<div class="">
-  <div v-if="error">
-    {{ error }}
-  </div>
-  <div v-else>
-    <div class="container mx-auto flex flex-wrap items-center justify-center p-6">
-      <nuxt-link v-for="(category, key) in categories" :to="`/categories/${get(category,'slug','')}`" :key="key" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 ml-2 border border-gray-400 rounded shadow m-1">
-        {{ get(category, 'name', '') }}
-      </nuxt-link>
+  <div class="">
+    <div v-if="error">
+      {{ error }}
     </div>
-
+    <div v-else>
+      <div
+        class="container mx-auto flex flex-wrap items-center justify-center p-6"
+      >
+        <nuxt-link
+          v-for="(category, key) in categories"
+          :key="key"
+          :to="`/categories/${get(category, 'slug', '')}`"
+          class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 ml-2 border border-gray-400 rounded shadow m-1"
+        >
+          {{ get(category, "name", "") }}
+        </nuxt-link>
+      </div>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-import { get } from '~/utils/get'
+import { get } from "~/utils/get";
 
 export default {
   data() {
     return {
       categories: [],
-      error: null
-    }
-  },
-  methods: {
-    get
+      error: null,
+    };
   },
   async mounted() {
     try {
-      this.categories = await this.$strapi.find('categories')
+      this.categories = await this.$strapi.find("categories");
     } catch (error) {
-      this.error = error
+      this.error = error;
     }
   },
-}
+  methods: {
+    get,
+  },
+};
 </script>
 
-<style lang="css" scoped>
-</style>
+<style lang="css" scoped></style>
