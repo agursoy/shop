7672fa7ae59852bcd315fa89546712a622625bd2
Abdullah Gürsoy <a.gursoy@msn.com>
2020-11-02 15:23:05 +0300

    be updates

---
+++ frontend/pages/_custom.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-if="error">
+    {{ error }}
+  </div>
+  <div v-else>
+    <div v-html="page.body"></div>
+  </div>
+</template>
+<script>
+export default {
+  async fetch() {
+    try {
+      this.page = await this.$strapi.$pages.findOne({ slug: this.$route.path });
+    } catch (error) {
+      this.error = error;
+    }
+  },
+  data() {
+    return {
+      page: null,
+      error: null,
+    };
+  },
+};
+</script>
